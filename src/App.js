54d37cc5136aae977bafde468738e5962bc16b53
Drd4n <the_dann@hotmail.co.th>
2018-11-08 18:44:50 +0700

    [ADD]

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './App.css';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+        <center><div>
+          <img src
+          </div></center>
+          <div id="Sdiv">"HELLO SHIBA"</div>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
