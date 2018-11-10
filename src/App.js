430d95c2d67a4f66e48b7d56008d782e5c569d75
Drd4n <the_dann@hotmail.co.th>
2018-11-11 02:52:03 +0700

    Add Landing :Heart:

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Navbar from './Component/Navbar.js'
+import Landing from './Component/Landing.js'
 
 class App extends Component {
   render() {
@@ -8,6 +9,7 @@ class App extends Component {
       <div className="App">
      
       <Navbar />
+      <Landing />
 
       </div>
     );
