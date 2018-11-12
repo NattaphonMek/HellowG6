b7489a7a3db8d2c7d9ba743a2c2eaa89791cf610
Drd4n <the_dann@hotmail.co.th>
2018-11-12 16:14:11 +0700

    FixedNAVBAR:crying:

--- src/App.js
+++ src/App.js
@@ -7,19 +7,37 @@ import Navbar from './Component/Navbar.js'
 import Landing from './Component/Landing.js'
 import Who from './Component/Who.js'
 import Where from './Component/Where.js'
-// import When from './Component/When.js'
+import When from './Component/When.js'
 import FAQs from './Component/FAQs.js'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-      <Landing />
       <Navbar />
+      <div id="home">
+      <br />
+      <br />
+      <br />
+      
+      
+      <Landing />
+      </div>
+      <div id="what">
       <What/>
+      </div>
+      <div id="where">
       <Where/>
+      </div>
+      <div id="who">
       <Who/>
+      </div>
+      <div id="when">
+      <When/>
+      </div>
+      <div id="faqs">
       <FAQs/>
+      </div>
        
       </div>
     );
--- src/Component/When.js
+++ src/Component/When.js
@@ -2,32 +2,98 @@ import React, { Component } from 'react'
 import styled from 'styled-components'
 
 const Body = styled.div`
-  height:100vh;
-    width:100vw;
+background: url('Images/when/PixelArt1.png') #df6d6a;
+position: center;
+height:100vh;
+width:100vw;
+background-size: cover;
+`
+const Head = styled.h1`
+padding : 12px
+color : white;
+text-align : center
+font-weight: bold
+font-family: 'Press Start 2P', cursive;
+`
+const Images = styled.img`
+height: 550px;
+
+`
+const Box = styled.div`
+`
+const M = styled.img`
+height: 130px;
+width: 130px;
+float: left;
+`
+const M2 = styled.img`
+height: 150px;
+width: 150px;
+float: right;
 `
 
+const Head2 = styled.div`
+color : white;
+padding : 10px
+font-size : 20px
+text-align : center
+font-weight: bold
+font-family: 'Press Start 2P', cursive;
+`
 
-
-
-class When extends Component {
-    render() {
-        return (
-         
-                     
-                <div className="">
-                
-                  When
-                  
-                
-                  
-                  
-                </div>
+const When =(props) => (
+  <Body>
+      <br></br>
+      <br></br>
+      <Box className="container">
+          <div className="row">
+              <div class="col-lg-5">
+              <M2 src="/Images/when/98f82fc462adcd7.png"/>
+                  <Head>
+                      <br></br>
+                      WHEN
+                  </Head>
+              </div>
+              <div class="col-lg-7">
+              </div>
+          <div className="row">
+              <Box class="col-lg-5">
+                  <Images src="/Images/where/Shibadrafwhere.png"/>
+              </Box>
+              <Box class="col-md-2">
+             <div className="rowpic">
+             <M src="./Images/download (6).png"/>
+             </div>
+             <div className="rowpic">
+                <M src="./Images/download (6).png"/>
+             </div>
+               <div className="rowpic">
+               <M src="./Images/download (6).png"/>
+             </div>
+             <div className="rowpic">
+               <M src="./Images/download (6).png"/>
+             </div>
+            </Box>
+            <Box class="col-md-5">
+            <br></br>
+                <Head2> เปิดรับสมัคร<br></br>10 ต.ค. 61 </Head2>
+            <br></br>
+            <br></br>
+                <Head2> ปิดรับสมัคร<br></br>11 ต.ค. 61 </Head2>
+                <br></br>
+            <br></br>
+                <Head2> เปิดกิจกรรม<br></br>12 ต.ค. 61 </Head2>
+                <br></br>
+                <br></br>
+                <Head2> ปิดกิจกรรม<br></br>13 ต.ค. 61 </Head2>           
+            </Box>
               
-          
-        );
-      }
-  }
-  
-
-
+              </div>    
+          </div>
+      </Box>
+      <br></br>
+      </Body>    
+    
+      )
+      
 export default When;
